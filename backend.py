abcab9ed6a519de123760ece86b885f181b6ea7f
Annika <annika@worldbrightening.net>
2026-09-11 23:42:47 -0700

    First try at plotting

--- backend.py
+++ backend.py
@@ -18,7 +18,11 @@
 import argparse
 import time
 import sqlite3
+import datetime 
+import base64
+
 from http.server import *
+from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(
     prog='backend.py',
@@ -43,14 +47,88 @@ CREATE TABLE IF NOT EXISTS weather_data (
 db.cursor().executescript(DB_INITIALIZATION_SQL).close() 
 db.commit()
 
+def heat_index(temperature_C, RH):
+    # COmpute heat index in degrees Celsius from temperature in degrees Celsius and relative humidity in percent
+    # Formula from USA National Weather Service: https://www.weather.gov/ama/heatindex
+    T = temperature_C * 9/5 + 32
+    hi_F = -42.379 + (2.04901523*T) + (10.14333127*RH) - (.22475541*T*RH) - (.00683783*T*T) - (.05481717*RH*RH) + (.00122874*T*T*RH) + (.00085282*T*RH*RH) - (.00000199*T*T*RH*RH)
+    return (hi_F - 32) * (5/9)
+
+HIGH = -274
+HIGHHI = -274
+LOW = -274
+LOWHI = -274
+
+def update_plots():
+    # Regenerates the plots of temperature, humidity, and heat index over the last 24 hours
+    cursor = db.cursor()
+    # Get the last 24 hours of data
+    cursor.execute('SELECT timestamp, temperature, humidity FROM weather_data WHERE timestamp >= ?', (time.time() - 24*60*60,))
+    data = cursor.fetchall()
+    cursor.close()
+
+    times = [datetime.date.fromtimestamp(float(x[0])) for x in data]
+    temperatures = [float(x[1]) for x in data]
+    humidities = [float(x[2]) for x in data]
+    heat_indices = [heat_index(t, h) for t, h in zip(temperatures, humidities)]
+
+    HIGH = max(temperatures) if temperatures else None
+    LOW = min(temperatures) if temperatures else None
+    HIGHHI = max(heat_indices) if heat_indices else None
+    LOWHI = min(heat_indices) if heat_indices else None
+
+    fig, ax = plt.subplots()
+    ax.plot(times, temperatures, label='Temperature (C)')
+    ax.plot(times, heat_indices, label='Heat Index (C)')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('degrees Celsius')
+    # ax.set_xlim(datetime.date.fromtimestamp(time.time() - 24*60*60), datetime.date.fromtimestamp(time.time()))
+    ax.legend()
+    print(times, temperatures, humidities, heat_indices)
+    plt.savefig('plots/temp_last_24_hours.png')
+
+    # Plot humidity
+    fig, ax = plt.subplots()
+    ax.plot(times, humidities, label='Humidity (%)')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Relative Humidity (%)')
+    ax.legend()
+    plt.savefig('plots/humidity_last_24_hours.png')
+    print(data)
+
+update_plots()
+
+DEFAULT_HTML = """
+<!DOCTYPE html>
+<html>
+<body>
+    Today's High: $HIGH$&#176;C / $HIGHF$&#176;F<br>
+    Today's High Heat Index: $HIGHHI$&#176;C / $HIGHHIF$&#176;F<br>
+    Today's Low: $LOW$&#176;C / $LOWF$&#176;F<br>
+    Today's Low Heat Index: $LOWHI$&#176;C / $LOWHIF$&#176;F<br>
+    <img src="data:image/png;base64,$B64TEMP$" alt="Temperature (C)">
+    <img src="data:image/png;base64,$B64HUM$" alt="Humidity (%)">
+</body>
+</html>
+"""
+
 # Our own class extending BaseHTTPRequestHandler to handle POST requests
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Weather sensing backend. Go away')
-    
+
+        # open humidity/temp PNGs and encode them as base64 to embed in HTML
+        temp_png = open('plots/temp_last_24_hours.png', 'rb').read()
+        hum_png = open('plots/humidity_last_24_hours.png', 'rb').read()
+        b64temp = base64.b64encode(temp_png).decode('utf-8')
+        b64hum = base64.b64encode(hum_png).decode('utf-8')
+
+        html = DEFAULT_HTML.replace('$B64TEMP$', b64temp).replace('$B64HUM$', b64hum)
+        html = html.replace('$HIGH$', str(HIGH)).replace('$LOW$', str(LOW)).replace('$HIGHHI$', str(HIGHHI)).replace('$LOWHI$', str(LOWHI))
+        html = html.replace('$HIGHF$', str(HIGH * 9/5 + 32)).replace('$LOWF$', str(LOW * 9/5 + 32)).replace('$HIGHHIF$', str(HIGHHI * 9/5 + 32)).replace('$LOWHIF$', str(LOWHI * 9/5 + 32))
+        self.wfile.write(html.encode('utf-8'))
     def do_POST(self):
         # Check for secret key in headers
         if self.headers.get('X-Secret-Key') != args.secret :
@@ -75,6 +153,8 @@ class Server(BaseHTTPRequestHandler):
         cursor.execute('INSERT INTO weather_data (timestamp, temperature, humidity) VALUES (?, ?, ?)', (timestamp, temperature, humidity))
         cursor.close()
         db.commit()
+
+        update_plots()
 
         return self.send_response(200)
 
