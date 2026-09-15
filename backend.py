8c8407ecbc4b7c21d14b363048b70915ff86b6af
Annika <annika@worldbrightening.net>
2026-09-14 23:18:13 -0700

    Small fix

--- backend.py
+++ backend.py
@@ -122,6 +122,8 @@ def c_to_f(c):
 def f_to_c(f):
     return (f - 32) * 5/9
 def fmt_decimal(d):
+    if d is None:
+        return "None"
     return str(round(d, 1))
 
 # Our own class extending BaseHTTPRequestHandler to handle POST requests
